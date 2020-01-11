c1d385a79065af8df5b1ae8e4900018c607056b7
yagen-eater <j-hawk@hotmail.co.jp>
2020-01-12 08:53:04 +0900

    modify footer

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="mt-5">
         <nav class="navbar navbar-light bg-light justify-content-center">
-                <div class="navbar-brand m-0">© 2019 Movie Works</div>
+                <div class="navbar-brand m-0">© 2020 Movie Works</div>
         </nav>
 </footer>
